074500d8fd806f04f3052c3b5f2ffb47e3bd23cf
alf-byrnison <alfonsots09@gmail.com>
2020-11-14 13:02:49 -0300

    clase Financiera

---
+++ Grupal_M3_1/grupal_3_1.py
@@ -0,0 +1,8 @@
+###################Clase Cliente#######################
+
+
+################Clase Financiera#####################
+
+class Financiera:
+    def __init__(self):
+        self.nombre = nombre
