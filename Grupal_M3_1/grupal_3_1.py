39dd94b0b849547e4fd1cc2ffa7080b9d757a4f4
alf-byrnison <alfonsots09@gmail.com>
2020-11-14 13:08:21 -0300

    se arregla creacion de clase cliente

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -2,10 +2,10 @@
 
 
 class Cliente:
-    def __init__(self, nombre, id_unico, saldo)
-    self.nombre = nombre
-    self.id_unico = id_unico
-    self.saldo = saldo
+    def __init__(self, nombre, id_unico, saldo):
+        self.nombre = nombre
+        self.id_unico = id_unico
+        self.saldo = saldo
 
 ################Clase Financiera#####################
 
