f57b2ef483bb8de01f407259a6ce6b68abd551fd
alf-byrnison <alfonsots09@gmail.com>
2020-11-14 13:57:22 -0300

    se corrijen metodos

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -12,20 +12,25 @@ class Cliente:
 
 class Financiera:
     def __init__(self):
-        self.nombre = nombre
-        self.id = id
+        self.nombre_financiera = nombre_financiera
+        self.id_financiera = id_financiera
         self.saldo_institucional = saldo_institucional
         self.clientes = clientes
+
     def agregar_cliente():
         return True
+
     def eliminar_cliente():
         return True
+
     def transferir():
         return True
+
     def giros_totales():
         return True
+
     def abonos_totales():
         return True
+
     def mostrar_saldo_institucional():
         return True
-        
