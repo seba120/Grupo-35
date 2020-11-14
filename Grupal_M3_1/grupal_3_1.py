520ac379cf8cb8e18c549ca1de9178797bb13eaa
emiajnipel <jlepinfernandez@gmail.com>
2020-11-14 15:48:50 -0300

    test metodo cliente

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -1,25 +1,49 @@
-# se solicitan crear las clases Cliente y Financiera.
+################### Clase Cliente #######################
 
-# clase cliente
 class Cliente:
-    def __init__ (self, nombre, id, saldo):
+    def __init__(self, saldo_cliente):
         self.nombre = nombre
-        self.id = id
-        self.saldo
+        self.rut = rut
+        self.saldo = saldo_cliente
+
+    def girar(self, cantidad):
+        #realiza un giro en la cuenta
+        self.saldo -= cantidad
+        return self.saldo
+
+    def abonar(self, cantidad):
+        # realiza un abono en la cuenta
+        self.saldo += cantidad
+        return self.saldo
+
+    def mostrar_saldo(self):
+        # retorana el balance del saldo
+        return self.saldo
 
 
+################ Clase Financiera #####################
 
 
-
-# clase financiera
 class Financiera:
-    def __init__ (self, nombre, id, saldo_institucional, cliente):
+    def __init__(self):
         self.nombre = nombre
-        self.id = id
+        self.rut = rut
         self.saldo_institucional = saldo_institucional
-        self.cliente =
+        self.clientes = clientes
 
 
+    def agregar_cliente():
+        return True
+    def eliminar_cliente():
+        return True
+    def transferir():
+        return True
 
+    def giros_totales(self):
+        return True
 
+    def abonos_totales(self):
+        return True
 
+    def mostrar_saldo_institucional():
+        return True
