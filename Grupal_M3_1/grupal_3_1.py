a781a0af5a8d7b7749fc22a4f25e72b8e684efc1
seba120 <seba120@users.noreply.github.com>
2020-11-14 13:48:11 -0300

    clase cliente

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -2,7 +2,7 @@
 
 
 class Cliente:
-    def __init__(self, nombre, id_unico, saldo):
+    def __init__(self):
         self.nombre = nombre
         self.id_unico = id_unico
         self.saldo = saldo
