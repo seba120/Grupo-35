634eab98e6959048891698ee13705f547d9dba00
Kattyabo <k.barrera.ordenes@gmail.com>
2020-11-14 13:43:54 -0300

    Se crea clase financiera, con sus funciones

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -13,3 +13,19 @@ class Cliente:
 class Financiera:
     def __init__(self):
         self.nombre = nombre
+        self.id = id
+        self.saldo_institucional = saldo_institucional
+        self.clientes = clientes
+    def agregar_cliente():
+        return True
+    def eliminar_cliente():
+        return True
+    def transferir():
+        return True
+    def giros_totales():
+        return True
+    def abonos_totales():
+        return True
+    def mostrar_saldo_institucional():
+        return True
+        
