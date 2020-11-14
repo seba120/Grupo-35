795ac2c8e661f04ef2e10236e2b6eee8312f7ddb
seba120 <seba120@users.noreply.github.com>
2020-11-14 14:58:39 -0300

    comentarios clase Cliente

--- Grupal_M3_1/grupal_3_1.py
+++ Grupal_M3_1/grupal_3_1.py
@@ -1,29 +1,29 @@
 ###################Clase Cliente#######################
 
-class Cliente:
-    def __init__(self, nombre_cliente, id_cliente , saldo_cliente):
-        self.nombre_cliente = nombre_cliente
-        self.id_cliente = id_cliente
-        self.saldo_cliente = saldo_cliente
+class Cliente: #Se crea la clase cliente
+    def __init__(self, nombre_cliente, id_cliente , saldo_cliente): #se define como parámetros iniciales nombre de cliente, id de cliente y saldo.
+        self.nombre_cliente = nombre_cliente #Se define que nombre cliente se asignará a futuro.
+        self.id_cliente = id_cliente #Se define que el id de cliente se asignará a futuro.
+        self.saldo_cliente = saldo_cliente #Se define que el saldo de cliente se asignará a futuro.
 
     #Metodo Clase Cliente
-    def girar(self , cantidad_giro):
-        self.cantidad_giro = cantidad_giro
-        self.saldo_cliente = self.saldo_cliente - self.cantidad_giro
-        return self.cantidad_giro
+    def girar(self , cantidad_giro): #Se define el parámetro cantidad de giro
+        self.cantidad_giro = cantidad_giro #Se define que cantidad de giro se asignará a futuro.
+        self.saldo_cliente = self.saldo_cliente - self.cantidad_giro #Se define que el saldo cliente que se solicite a futuro será el saldo menos la cantidad girada.
+        return self.cantidad_giro #se solicita que la función otorgue como resultado la cantidad de giro.
 
-    def abonar(self , cantidad_abono):
-        self.cantidad_abono = cantidad_abono
-        self.saldo_cliente = self.saldo_cliente + self.cantidad_abono
-        return self.cantidad_abono
+    def abonar(self , cantidad_abono): #se define la función abonar con el parámetro a definir de cantidad de abono. 
+        self.cantidad_abono = cantidad_abono #se define que cantidad de abono será asignado a futuro.
+        self.saldo_cliente = self.saldo_cliente + self.cantidad_abono #se define que el saldo del cliente será dicho monto mas la cantidad abonada.
+        return self.cantidad_abono #la función arrojará como resultado la cantidad abonada.
 
-    def mostrar_saldo (self):
-        return "El saldo de {} es: {}".format(self.nombre_cliente , self.saldo_cliente) 
+    def mostrar_saldo (self): #función para mostrar saldo sin parámetros por asignar.
+        return "El saldo de {} es: {}".format(self.nombre_cliente , self.saldo_cliente) #se define que el resultado de la función será un str predefinido con dos self a llamar: nombre de cliente + saldo de cliente.
 
-seba = Cliente("seba" , 1 , 1000000)
-print("Usted giró:" , seba.girar(300000) , ". El saldo de su cuenta es:" , seba.saldo_cliente , "." )
 
-print(seba.mostrar_saldo())
+
+
+
 
 ################Clase Financiera#####################
 
