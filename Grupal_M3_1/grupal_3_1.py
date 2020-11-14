5bcc89a4d42a6b06e92d377fc4a56662d1a7187e
emiajnipel <jlepinfernandez@gmail.com>
2020-11-14 13:15:42 -0300

    creacion de clases

---
+++ Grupal_M3_1/grupal_3_1.py
@@ -0,0 +1,20 @@
+# se solicitan crear las clases Cliente y Financiera.
+# clase cliente
+
+class Cliente:
+    def __init__ (self, nombre, id, saldo):
+        self.nombre = nombre
+        self.id = id
+        self.saldo
+
+class Financiera:
+    def __init__ (self, nombre, id, saldo_institucional, cliente):
+        self.nombre = nombre
+        self.id = id
+        self.saldo_institucional = saldo_institucional
+        self.cliente =
+
+
+
+# clase financiera
+
