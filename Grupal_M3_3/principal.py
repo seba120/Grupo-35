55778d95cea38e394196980dfb8b90db4558aef2
alf-byrnison <alfonsots09@gmail.com>
2020-11-25 09:49:57 -0300

    Se agregan comentarios y corrige funciones de Condominio

--- Grupal_M3_3/principal.py
+++ Grupal_M3_3/principal.py
@@ -3,50 +3,65 @@ import random
 
 ####### Condominio ########################
 
+# Clase principal condominio, con su constructor
+
 
 class Condominio:
-    def __init__(self):
-        self.direccion = ""
+    def __init__(self):  # la clase no tiene parametros ademas de self
+        self.direccion = ''
+        # en los casos de lista_administrador, lista_guardias y
         self.lista_administrador = []
-        self.lista_guardias = []
+        self.lista_guardias = []  # lista_unidades se inicializa como listas vacias
         self.lista_unidades = []
+        # numero de unidades habitacionales se cuenta como el largo de la lista_unidades
         self.numero_unidades_habitacionales = len(self.lista_unidades)
+        # la cuenta corriente se da como un id unico al azar
         self.cuenta_corriente = uuid.uuid4()
+        self.nombre_condominio = ''  # nombre_condominio se inicializa como vacio
 
+    # funcion pide por parametro nombre del condominio
+    def set_nombre_condominio(self, nombre_condominio):
+        self.nombre_condominio = nombre_condominio
+        return self.nombre_condominio  # retorna el nombre ingresado
+
+    # funcion setea la direccion del objeto instanciado y pide la direccion como str por parametro
     def set_direccion(self, direccion):
         self.direccion = direccion
         return self.direccion
 
     def get_direccion(self):
-        return self.direccion
+        dato = 'La direccion del condominio {} es {}'  # da el formato de salida
+        # devuelve la direccion concatenada con
+        return dato.format(self.nombre_condominio, self.direccion)
+        # nombre del condominio
 
-    def set_administrador(self , nombre_administrador):
+    # funcion agrega administradores al condominio
+    def set_administrador(self, nombre_administrador):
         self.nombre_administrador = nombre_administrador
-        self.lista_administrador = self.lista_administrador.append(self.nombre_administrador)
+        self.lista_administrador.append(self.nombre_administrador)
         return self.lista_administrador
 
+    # funcion retorna la lista_administrador con los administradores agregados
     def get_administrador(self):
-        print(self.lista_administrador)
+        return self.lista_administrador
 
-    # agrega guardia por parametro
+    # agrega guardia por parametro y lo guarda en una variable para ser agregado a lista_guardias
     def add_guardias(self, guardia):
-        if len(self.lista_guardias) >= 4:
-            print('No es posible agregar mas guardias')
-        else:
-            self.guardia = guardia
-            self.lista_guardias.append(self.guardia)
-        return self.lista_guardias
+        self.guardia = guardia
+        self.lista_guardias.append(self.guardia)
+        return self.lista_guardias  # retorna la lista actualizada
 
-    # elimina guardia entregado por parametro
+    # elimina guardia entregado por parametro desde la lista_guardias
     def del_guardias(self, guardia):
         self.guardia = guardia
         self.lista_guardias.remove(self.guardia)
-        return self.lista_guardias
+        return self.lista_guardias  # retorna la lista actualizada
 
-    # muestra los guardias del condominio
+    # muestra una lista de los guardias del condominio mediante un for con el formato indicado
     def get_guardias(self):
+        print('\n---- Listado de guardias ----')
         for i in range(len(self.lista_guardias)):
-            print(edificio.lista_guardias[i].nombre,
+            print('Guardia', (i+1), edificio.lista_guardias[i].nombre,
                   edificio.lista_guardias[i].apellido)
 
     def get_unidades(self):
@@ -55,18 +70,24 @@ class Condominio:
 
 ############ Guardia #############################
 
-class Guardia:
+# clase Guardias se le asignan atributos, solo se solicitan nombre y apellido como parametro
+class Guardia:  # en el constructor
     def __init__(self, nombre, apellido):
         self.nombre = nombre
         self.apellido = apellido
-        self.__id = uuid.uuid1()
+        self.__id = uuid.uuid1()  # se le da un id unico y es privado
+        # se setea un sueldo base y es privado al igual que horas extras
         self.__sueldo_base = 500000
+        # se agrega un atributo de horas extra como random(0-40)
         self.__horas_extra = random.randint(0, 40)
+        # atributo de tiurno elije al azar entre dia y noche
         self.turno = random.choice(['Dia', 'Noche'])
 
+    # metodo para acceder al id privado del guardia
     def get_id(self):
         return self.__id
 
+    # metodo para entrgar datos de sueldo calculado
     def get_sueldo(self):
         valor_hora = (self.__sueldo_base/40)/4
         total_horas = valor_hora * self.__horas_extra
@@ -85,7 +106,7 @@ class Guardia:
 
 ############ Cuenta corriente ###################
 # class Cuenta Corriente (con al menos 3 atributos y 4 métodos)
-
+'''
 class Cuenta_corriente:
     def __init__(self, ingresos, gastos):
         self.ingresos = random.randint(15000000, 18000000)
@@ -140,10 +161,14 @@ class Terreno:
     def demoler(self):
         pass
 
+'''
 
 ######### Ejecuciones de prueba ##########
 # se crea edificio
 edificio = Condominio()
+# se setea nombre del objeto
+edificio.set_nombre_condominio('El Rozal')
+
 # se crean guardias
 guardia1 = Guardia('Juan', 'Perez')
 guardia2 = Guardia('Lalo', 'Landas')
@@ -178,4 +203,4 @@ print(edificio.get_direccion())
 
 print()
 edificio.set_administrador("Administrador2")
-edificio.get_administrador()
+print(edificio.get_administrador())
